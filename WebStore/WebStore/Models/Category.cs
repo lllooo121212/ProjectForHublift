2656ed709177278ae11980377014e92602fd0a79
Twannnnnn <hkt93885@gmail.com>
2024-11-20 04:45:15 +0700

    product and category manage

--- WebStore/WebStore/Models/Category.cs
+++ WebStore/WebStore/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -14,7 +15,12 @@ namespace WebStore.Models
             Product = new HashSet<Product>();
         }
 
+        [Required(ErrorMessage = "Category ID is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Category ID must be a positive number.")]
         public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Category Name is required.")]
+        [StringLength(100)]
         public string CategoryName { get; set; }
 
         public virtual ICollection<Product> Product { get; set; }
