c0a0d1e07c60b196f66406e36d72640c7908bc10
lộc trương <noneednowok@gmail.com>
2024-11-20 03:31:21 +0700

    Initial commit

---
+++ WebStore/WebStore/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        private StoreDbContext db = new StoreDbContext();
+
+        public ActionResult Index()
+        {
+            var products = db.Product.ToList();
+            return View(products);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
